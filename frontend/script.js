a71329cdb4764132a7b6fedbe7cc7566627dfe31
joepk8 <joepjfklinkenberg@gmail.com>
2023-04-13 21:38:36 +0200

    werk

--- frontend/script.js
+++ frontend/script.js
@@ -5,20 +5,6 @@ L.tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
 }).addTo(map);
 
-// var marker = L.marker([51.924578, 4.478971]).addTo(map);
-
-
-
-
-// marker.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
-// circle.bindPopup("I am a circle.");
-// polygon.bindPopup("I am a polygon.");
-
-
-// var popup = L.popup()
-//     .setLatLng([51.513, -0.09])
-//     .setContent("I am a standalone popup.")
-//     .openOn(map);
 
 function onMapClick(e) {
     alert("U heeft hier op de map geklikt: " + e.latlng);
