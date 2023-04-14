d6e01afc777431eef20d91e686b961b0dc468eb8
joepk8 <joepjfklinkenberg@gmail.com>
2023-04-14 03:32:46 +0200

    summery

---
+++ frontend/search.js
@@ -0,0 +1,33 @@
+fetch("shadespotSearch.json")
+  .then(response => response.json())
+  .then(data => {
+    console.log(data);
+    // Do something with the data here
+  })
+  .catch(error => console.log(error));
+//  function search(event) {
+//     event.preventDefault(); // voorkomt dat het formulier wordt verzonden
+  
+//     const input = document.getElementById("search-bar").value.toLowerCase();
+  
+//     const results = searchData.filter(obj =>
+//       Object.values(obj).some(val =>
+//         typeof val === "string" && val.toLowerCase().includes(input)
+//       )
+//     );
+  
+//     const resultContainer = document.getElementById("results");
+//     resultContainer.innerHTML = "";
+  
+//     if (results.length > 0) {
+//       const resultList = document.createElement("ul");
+//       results.forEach(obj => {
+//         const listItem = document.createElement("li");
+//         listItem.textContent = `${obj.name} (${obj.age})`;
+//         resultList.appendChild(listItem);
+//       });
+//       resultContainer.appendChild(resultList);
+//     } else {
+//       resultContainer.textContent = "Geen resultaten gevonden.";
+//     }
+//   }
